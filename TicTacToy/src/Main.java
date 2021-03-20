b8f5779e0a6504659af98946ded58232235358f2
Julia Onix <julia.mur2mur@gmail.com>
2021-03-20 12:05:30 +0300

    I did that commit because Anastasia had asked me about it :))
    Here I made some cycles and tried to find the winner.

--- TicTacToy/src/Main.java
+++ TicTacToy/src/Main.java
@@ -1,16 +1,95 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args) {
 
         String[][] array = {{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
+        Scanner scanner = new Scanner(System.in);
+        String  prompt  = "";
+        String  greeting = "Hello! The first turn by [X].";
+        boolean isFinish = true;
+        int counterX = 0;
 
-        //show current table
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " | ");
+        while (isFinish) {
+            System.out.println(greeting);
+
+            //select cell and show current table
+
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    if (j == 0) {
+                        System.out.print(" | ");
+                    }
+
+                    System.out.print(array[i][j] + " | ");
+                }
+                System.out.println("");
             }
-            System.out.println("");
-        }
 
+            System.out.println("[x]'s turn, select cell");
+
+            prompt = scanner.nextLine();
+
+            //got a number and look for position
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    if (array[i][j].equals(prompt)) {
+                        array[i][j] = "X";
+                    }
+                }
+            }
+
+            System.out.println("[o]'s turn");
+
+            //show current table
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    if (j == 0) {
+                        System.out.print(" | ");
+                    }
+
+                    System.out.print(array[i][j] + " | ");
+                }
+                System.out.println("");
+            }
+
+            prompt = scanner.nextLine();
+
+            //got a number and look for position
+            for (int i = 0; i < array.length; i++)
+            {
+                for (int j = 0; j < array[i].length; j++)
+                {
+                    if (array[i][j].equals(prompt))
+                    {
+                        array[i][j] = "O";
+                    }
+                }
+            }
+
+
+
+            //Is winner?
+            for (int i = 0; i < array.length; i++)
+            {
+                for (int j = 0; j < array[i].length; j++)
+                {
+                    /*X | X | X
+                    * 4 | 5 | 6
+                    * 7 | 8 | 9*/
+
+                    if (array[0][j].equals("X"))
+                    {
+                        counterX++;
+
+                        if ( counterX == 3)
+                        {
+                            System.out.println("You have won!");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
